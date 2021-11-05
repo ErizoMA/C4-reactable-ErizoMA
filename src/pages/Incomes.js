370fdfc7f84735dbd523ee9c42464ad809a5eb3d
Erick <marcerican@gmail.com>
2021-11-05 12:46:16 -0500

    Create components of Incomes page

--- src/pages/Incomes.js
+++ src/pages/Incomes.js
@@ -1,5 +1,17 @@
+import { WhiteButton } from "../components/UI/Button";
+import Card from "../components/UI/Card";
+import { ActiveTab, InactiveTab } from "../components/UI/Tab";
+import Title from "../components/UI/Title";
 function Incomes() {
-  return <div>Incomes</div>;
+  return (
+    <div>
+      <Title title="Expensable" />
+      <InactiveTab title="Expenses" />
+      <ActiveTab title="Incomes" />
+      <Card category="Salary" count="2" total="5000.00" />
+      <WhiteButton>Log out</WhiteButton>
+    </div>
+  );
 }
 
 export default Incomes;
